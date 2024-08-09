87ca863c6b9deda9e2c826c6af7d1a7b0b2b9203
Lilyltt <1806552019@qq.com>
2024-08-10 01:30:25 +0800

    First UI Design

---
+++ Lagrange.Desktop/DivideByConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Lagrange.Desktop;
+
+public class DivideByConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is double width && parameter is string ratioString && double.TryParse(ratioString, out double ratio))
+        {
+            return width * ratio;
+        }
+        return 0;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
