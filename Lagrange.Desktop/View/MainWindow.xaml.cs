a1fb79edae480c06f1af47c3c9074c76de235093
Lilyltt <1806552019@qq.com>
2024-08-10 01:30:25 +0800

    First UI Design

--- Lagrange.Desktop/View/MainWindow.xaml.cs
+++ Lagrange.Desktop/View/MainWindow.xaml.cs
@@ -1,24 +1,32 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Lagrange.Desktop.ViewModel;
+using Panuon.WPF.UI;
 
-namespace Lagrange.Desktop
+namespace Lagrange.Desktop.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : WindowX
     {
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
+            SetWindowXCaptionBinding();
+        }
+        private void SetWindowXCaptionBinding()
+        {
+            var binding = new Binding
+            {
+                Path = new PropertyPath("ActualHeightProperty"),
+                RelativeSource = new RelativeSource(RelativeSourceMode.Self),
+                Converter = new DivideByConverter(),
+                ConverterParameter = 0.3
+            };
+
+            SetBinding(WindowXCaption.HeightProperty, binding);
         }
     }
 }
---
+++ Lagrange.Desktop/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Lagrange.Desktop.ViewModel;
+
+public class MainWindowViewModel : ObservableObject
+{
+    public class DivideByFourConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double width)
+            {
+                return width / 4;
+            }
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
